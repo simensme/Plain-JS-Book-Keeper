73c3426f0bc932a3d9a842227e502aadf9282db9
Simen Smehaugen <simdevgen@outlook.com>
2023-11-30 15:21:26 +0100

    added regex checks to url & name

--- script.js
+++ script.js
@@ -10,14 +10,51 @@ const bookmarksContainer = document.getElementById('bookmarks-container');
 const showModal = () => {
     modal.classList.add('show-modal');
     websiteNameEl.focus();
-}
+};
 
 // Close Modal
 const closeModal = () => {
     modal.classList.remove('show-modal');
-}
+};
 
 // Modal Event Listeners
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', closeModal);
 window.addEventListener('click', e => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate the form
+const validate = (nameValue, urlValue) => {
+    // Expression to check that the web address has a valid format, e.g no spacing
+    const expression = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue){
+        alert('Please add values to both fields');
+        return false;
+    }
+    if (!urlValue.match(regex)){
+        alert('Please provide a valid web address');
+        return false;
+    }
+    // if everything is ok
+    return true;
+};
+
+// Handle data from form
+const storeBookmark = e => {
+    // Prevent it from submitting a form to a web-server
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+        urlValue = `https://${urlValue}`;
+    }
+    if(!validate(nameValue, urlValue)){
+        return false;
+    } 
+};
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
+
+
+
